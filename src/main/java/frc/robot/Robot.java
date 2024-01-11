5110e8c79b7ea0126f4ae05d45fcb2f63dc9c1d4
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-01-10 19:35:08 -0500

    feat: upped speed to 14m/s and 1x drivevals

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -105,7 +105,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     double[] driveVals = controller.getControllerDrive();
-    drivetrain.drive(0.2 * driveVals[0], 0.2 * driveVals[1], 0.2 * driveVals[2], false);
+    drivetrain.drive(1 * driveVals[0], 1 * driveVals[1], 1 * driveVals[2], false);
   }
 
   /** This function is called once when the robot is disabled. */
