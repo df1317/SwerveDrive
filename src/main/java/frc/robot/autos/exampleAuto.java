c93695ed6d8afdeef6fe5c3d40166c260afa6623
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-01-15 20:15:32 -0500

    feat: added pathplannerlib

--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -29,9 +29,9 @@ public class exampleAuto extends SequentialCommandGroup {
             // Start at the origin facing the +X direction
             new Pose2d(0, 0, new Rotation2d(0)),
             // Pass through these  interior waypoints
-            List.of(new Translation2d(3, 0)), 
+            List.of(new Translation2d(0, -3), new Translation2d(-3, -3)), 
             // End 1.5 meters straight ahead of where we started, facing forward
-            new Pose2d(1.5, 0, new Rotation2d(0)),
+            new Pose2d(3, 0, new Rotation2d(0)),
             config);
 
     var thetaController =
